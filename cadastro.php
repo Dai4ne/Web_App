767e842a9fd4b329706022f8a6b8aed4489ddaac
Daiane <daianeksilva07@gmail.com>
2025-09-08 00:58:14 -0300

    Atualização 1
    Colocando os arquivos que fiz da tela de login e de cadastro

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Cadastro</h1>
+
+        <input type="text" placeholder="Nome">
+        <input type="text" placeholder="E-mail">
+        <input type="password" placeholder="Senha">
+        <input type="text">
+        <button>Enviar</button>
+    </div>
+</body>
+</html>
